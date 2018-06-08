fcc16c89950089772ed4c9d4594c0c6a7aa1cbf0
chaim <wuliangcheng86@icloud.com>
2018-06-08 21:35:09 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -63,15 +63,52 @@ install_docker() {
     os=$(. /etc/os-release; echo "${ID}")
     lsb_release=$(lsb_release -cs)
 
-    curl -fsSL "${DOCKER_DOWNLOAD}/${os}/gpg" | sudo apt-key add -
+   # curl -fsSL "${DOCKER_DOWNLOAD}/${os}/gpg" | sudo apt-key add -
+   curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+    
+   # add-apt-repository \
+    #    "deb [arch=amd64] ${DOCKER_DOWNLOAD}/${os} ${lsb_release} stable"
     
     add-apt-repository \
-        "deb [arch=amd64] ${DOCKER_DOWNLOAD}/${os} ${lsb_release} stable"
+    "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+    $(lsb_release -cs) stable"
+        
+    cat >>/etc/apt/sources.list<<EOF
+# deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
+deb-src http://archive.ubuntu.com/ubuntu xenial main restricted #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse #Added by software-properties
+deb http://archive.canonical.com/ubuntu xenial partner
+deb-src http://archive.canonical.com/ubuntu xenial partner
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security multiverse
+EOF
 
     apt-get update
     apt-get install -y docker-ce
 
     usermod -aG docker ubuntu
+    
+    # add mirror for china uers
+    cat >>/etc/docker/daemon.json<<EOF
+    {
+        "registry-mirrors": ["https://registry.docker-cn.com"]
+    }
+    EOF
+   
+    systemctl daemon-reload
+    systemctl restart docker
+    
 }
 
 # ------------------------------------------------------------------------------
@@ -98,7 +135,7 @@ install_netdata() {
 # ------------------------------------------------------------------------------
 install_portainer() {
     mkdir -p /usr/share/portainer
-
+    
     docker pull portainer/portainer:latest
 
     docker create \
@@ -125,6 +162,7 @@ install_portainer() {
 install_hassio() {
     curl -sL "${HASSIO_INSTALLER}" | bash -s
 }
+
 
 # ------------------------------------------------------------------------------
 # Shows a message on how to connect to Home Assistant, including the
